af4f1b6bd5073a6638324b6a2698f4fdb3ac67f6
smurfpy <mugenxper@hotmail.com>
2020-04-11 17:15:33 +0700

    Edit Mega 2560

--- CutterBot/Beta Code/Mega2560/Mega2560.ino
+++ CutterBot/Beta Code/Mega2560/Mega2560.ino
@@ -6,14 +6,16 @@ AF_DCMotor motor2(2);
 AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
 String inString;
-
+#define pin 24
 void setup() {
   Serial.begin(115200);
   Serial3.begin(115200);
-  motor1.setSpeed(200);
-  motor2.setSpeed(200);
-  motor3.setSpeed(200);
-  motor4.setSpeed(200);
+  motor1.setSpeed(255);
+  motor2.setSpeed(255);
+  motor3.setSpeed(255);
+  motor4.setSpeed(255);
+  pinMode(pin,OUTPUT);
+  digitalWrite(pin,LOW);
 }
 
 void loop() 
@@ -22,6 +24,7 @@ void loop()
 }
 
 void serialEvent3() {
+  digitalWrite(pin,LOW);
   while (Serial3.available()) {
     char inChar = Serial3.read();
     Serial.write(inChar);
@@ -42,7 +45,9 @@ void serialEvent3() {
         motor4.run(BACKWARD);
       }
       else if (inString.indexOf("[RIGHT]")>0) 
-      {
+      { 
+        motor1.run(RELEASE);
+        motor2.run(RELEASE);
         motor3.run(FORWARD);
         motor4.run(FORWARD);
       }
@@ -50,13 +55,23 @@ void serialEvent3() {
       {
         motor1.run(FORWARD);
         motor2.run(FORWARD);
+        motor3.run(RELEASE);
+        motor4.run(RELEASE);
       }
       else if (inString.indexOf("[STOP]")>0) 
       {
-        motor1.run();
-        motor2.run();
-        motor3.run();
-        motor4.run();
+        motor1.run(RELEASE);
+        motor2.run(RELEASE);
+        motor3.run(RELEASE);
+        motor4.run(RELEASE);
+      }
+      else if (inString.indexOf("[ON]")>0) 
+      {
+        digitalWrite(pin,HIGH);
+      }
+      else if (inString.indexOf("[OFF]")>0) 
+      {
+        digitalWrite(pin,LOW);
       }
       inString = "";
     }
